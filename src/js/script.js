b25fb996934031c0cb804adf38f02c23fc86a376
D <morqqulis@gmail.com>
2025-09-22 05:17:12 +0400

    okay

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,12 @@
 // Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./files/functions.js";
+// import { isMobile } from "./files/functions.js";
 // Подключение списка активных модулей
-import { flsModules } from "./files/modules.js";
+// import { flsModules } from "./files/modules.js";
+
+const moreBtn = document.querySelector('.about__more-btn');
+if (moreBtn) {
+	moreBtn.addEventListener('click', () => {
+		const more = document.querySelector('.about__more');
+		more.classList.toggle('about__more_active');
+	});
+}
